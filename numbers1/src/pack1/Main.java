4a010d1c3011d1c7925e0b2c7dc583bd655afc9d
KaterynaPukha <KatherynaPukha@gmail.com>
2019-02-06 13:10:39 +0200

    6.02

--- numbers1/src/pack1/Main.java
+++ numbers1/src/pack1/Main.java
@@ -3,11 +3,18 @@ package pack1;
 public class Main {
 
     public static void main(String[] args) {
-       int x = 67;
+        int a,b;
+        int x = 67;
        int y = 3;
        System.out.println(x+y);
        System.out.println(x-y);
        System.out.println(x*y);
-       System.out.println(x/y);
+       System.out.println("Частное от диление" +  (double)x/y);
+       a=x++;
+       b=++x;
+       System.out.println (a);
+       System.out.println (b);
+       x--;
+       --x
     }
 }
