095c57e9b4891c2ed41056f04003210b267929ac
KaterynaPukha <Katherynapukha@gmail.com>
2019-02-26 23:39:09 +0200

    Hw_26/02

---
+++ Hw_26.02/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    for (int i=0; i<=40; i++){
+            System.out.print( i + " ");
+        }
+        System.out.println();
+	    for (int k=0; k<=3; k++){
+	        for (int l=0; l<=9; l++){
+                System.out.print(10*k + l + " ");
+            }
+            System.out.println();
+        }
+    }
+}
